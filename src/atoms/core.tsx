d96a1578b08e57cad8724d51315849c02294c820
datt16 <osatom.4639@gmail.com>
2021-07-01 21:11:33 +0900

    :+1: ニューモーフィズムについて取り組み始めた

---
+++ src/atoms/core.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+`
+
+const Hero = styled.div`
+  min-height: 100vh;
+  padding: 5rem 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
+
+const Main = styled.main`
+  min-width: 960px;
+`
+
+const Footer = styled.footer`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 1;
+  }
+`
+
+const Title = styled.h1`
+  margin: 0;
+  line-height: 1.15;
+  font-size: 4rem;
+  text-align: center;
+  a {
+    color: #0070f3;
+    text-decoration: none;
+  }
+  a:hover,
+  a:focus,
+  a:active {
+    text-decoration: underline;
+  }
+`
+export { Container, Hero, Main, Footer, Title }
--- src/pages/styledComponent.tsx
+++ src/pages/styledComponent.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
-import styled from 'styled-components'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { monokaiSublime } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import { Container, Title, Footer, Hero, Main } from '../atoms/core'
 
 export default function StyledComponentPage() {
   return (
@@ -90,56 +90,3 @@ export default function styledComponent() {
     </div>
   )
 }
-
-/*-------------- style --------------*/
-
-const Container = styled.div`
-  padding: 1rem;
-  display: flex;
-  justify-content: center;
-`
-
-const Hero = styled.div`
-  min-height: 100vh;
-  padding: 5rem 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`
-
-const Main = styled.main`
-  min-width: 960px;
-`
-
-const Footer = styled.footer`
-  width: 100%;
-  height: 100px;
-  border-top: 1px solid #eaeaea;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  a {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-grow: 1;
-  }
-`
-
-const Title = styled.h1`
-  margin: 0;
-  line-height: 1.15;
-  font-size: 4rem;
-  text-align: center;
-  a {
-    color: #0070f3;
-    text-decoration: none;
-  }
-  a:hover,
-  a:focus,
-  a:active {
-    text-decoration: underline;
-  }
-`
